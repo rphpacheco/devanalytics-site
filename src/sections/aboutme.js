437c3d6554722151b0e3c147ebece6994c9b6455
rphpacheco <rphpacheco@gmail.com>
2021-10-18 18:02:52 -0300

    add aboutme section

--- src/sections/aboutme.js
+++ src/sections/aboutme.js
@@ -1,7 +1,7 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { rgba } from 'polished';
-import { jsx, Flex, Container, Heading, Text, Button } from 'theme-ui';
+import { jsx, Flex, Container, Heading, Text, Button, Link } from 'theme-ui';
 import Image from 'components/image';
 import Play from 'components/icons/play';
 import aboutme from 'assets/images/aboutme.png';
@@ -23,18 +23,32 @@ const AboutMe = () => {
             </Flex>
           </Flex>
           <Flex sx={styles.illustration.buttons}>
-            <Button
-              variant="outlined"
-              sx={styles.btnOutlined}
+            <Link
+              href="https://www.linkedin.com/in/raphael-pacheco-702a3b49/"
+              target="_blank"
+              alt="Raphael - LinkedIn"
+              sx={styles.link}
             >
-              LINKEDIN <BsLinkedin fill="#FF4D00" />
-            </Button>
-            <Button
-              variant="outlined"
-              sx={styles.btnOutlined}
+              <Button
+                variant="outlined"
+                sx={styles.btnOutlined}
+              >
+                LINKEDIN <BsLinkedin fill="#FF4D00" />
+              </Button>
+            </Link>
+            <Link
+              href="https://www.github.com/rphpacheco"
+              target="_blank"
+              alt="Raphael - GitHub"
+              sx={styles.link}
             >
-              GITHUB <GoMarkGithub fill="#FF4D00" />
-            </Button>
+              <Button
+                variant="outlined"
+                sx={styles.btnOutlined}
+              >
+                GITHUB <GoMarkGithub fill="#FF4D00" />
+              </Button>
+            </Link>
           </Flex>
         </Flex>
         <div sx={styles.content}>
@@ -44,12 +58,12 @@ const AboutMe = () => {
             <Text as="p">Raphael Pacheco é Analista de BI, FullStack Developer, consultor, músico  e 
             criador de conteúdo.
             Trabalha ná área de dados desde 2010, atuando em projetos para grandes
-            empresas como Santander, BR Distribuidora, Volkswagen, Mercedes Benz
+            empresas como Santander, BR Distribuidora, Volkswagen, Mercedes Benz,
             Algar Telecom, Thomson Reuters, Sebrae, Portobello e etc.
             Através do canal devAnalytics no YouTube, começou a compartilhar seu
             conhecimento adquirido em sua trajetória profissional e agora está 
             disponibilizando também cursos completos para quem quer atuar no ramo 
-            de desenvolvolvimento de aplicações web e análise e visualização de dados</Text>
+            de desenvolvolvimento de aplicações web e análise e visualização de dados.</Text>
         </div>
       </Container>
     </section>
@@ -59,6 +73,9 @@ const AboutMe = () => {
 export default AboutMe;
 
 const styles = {
+  link: {
+    textDecoration: "none"
+  },
   section: {
     backgroundColor: '#131313',
     // pt: [8, null, null, null, 10, 14],
